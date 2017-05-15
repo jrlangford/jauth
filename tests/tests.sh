c11d739a656c16d9c9306dc50ceaf5792c1ccf8b
Jonathan Robin Langford <jrobinlangford@gmail.com>
2017-05-15 02:38:40 -0500

    Update tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -9,3 +9,14 @@ function create_user_no_body {
 	curl -v -XPOST --header "Content-Type: application/json" localhost:8080/users
 }
 
+function log_in {
+	curl -v -c mycookie --header "Content-Type: application/json" -d @$DATA_DIR/goodLoginData.json localhost:8080/login
+}
+
+function log_in_bad_data {
+	curl -v -XPOST --header "Content-Type: application/json" -d @$DATA_DIR/badLoginData.json localhost:8080/login
+}
+
+function get_user_data {
+	curl -v -b mycookie localhost:8080/users/email/jrobin@gmail.com
+}
