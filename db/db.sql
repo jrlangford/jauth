c9c2c4d9542114b3081e7a0120b1b435ba727144
Jonathan Robin Langford <jrobinlangford@gmail.com>
2017-05-15 17:01:14 -0500

    Update db schema

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,9 @@
-create table p_user (
-	 id bigserial primary key,
-	 username text,
-	 fullname text,
-	 passwordhash text,
-	 passwordsalt text,
-	 isdisabled bool
+create table user_info (
+	id bigserial primary key,
+	username text,
+	fullname text,
+	passwordhash text,
+	passwordsalt text,
+	role text,
+	isdisabled bool
 );
